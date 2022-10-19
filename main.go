83c224fc43b0fdb24ecdc372dca6f3bd38ab69ba
root <root@vps-912380.battlebit.org>
2022-10-19 16:26:09 -0300

    only main

--- main.go
+++ main.go
@@ -1,28 +1,48 @@
-package main
+package chat
 
 import (
-    "chat"
-	"io/ioutil"
-	"log"
-    "net/http"
-	"github.com/gin-gonic/autotls"
-    "github.com/gin-gonic/gin"
-    "testing"
+	"fmt"
+	"os"
 	"github.com/Davincible/gotiktoklive"
 )
-func main() {
-initBotsaker()
-router := gin.Default()
-router.GET("/users", getusers)
-router.POST("/users", postusers)
 
-log.Fatal(autotls.Run(router, "botsaker.battlebit.org", "botsaker2.battlebit.org"))
-}
-func getusers(c *gin.Context) {
-	c.String(http.StatusOK, "pong")
-}
-func postusers(c *gin.Context) {
-	body, _ := ioutil.ReadAll(c.Request.Body)
-    println(string(body))
-	c.String(http.StatusOK, "pong")
+
+
+
+func initBotsaker() {
+
+	USERNAME := os.Getenv("USERNAME")
+	// Create TikTok Instance
+	tiktok := gotiktoklive.NewTikTok()
+
+	// Track a TikTok user by username
+	live, err := tiktok.TrackUser(USERNAME)
+	if err != nil {
+		panic(err)
+	}
+
+	// Start downloading stream
+	// Make sure you have the ffmpeg binary installed, and present in your path.
+	if err := live.DownloadStream(); err != nil {
+		panic(err)
+	}
+
+	// Receive livestream events through the live.Events channel
+	for event := range live.Events {
+		switch e := event.(type) {
+
+		// You can specify what to do for specific events. All events are listed below.
+		case gotiktoklive.UserEvent:
+			fmt.Printf("%T : %s %s\n", e, e.Event, e.User.Username)
+
+		// List viewer count
+		case gotiktoklive.ViewersEvent:
+			fmt.Printf("%T : %d\n", e, e.Viewers)
+
+		// Specify the action for all remaining events
+		default:
+			fmt.Printf("%T : %+v\n", e, e)
+		}
+	}	
+
 }
